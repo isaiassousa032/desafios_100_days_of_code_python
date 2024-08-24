d25f2560ce6e5e728cfa5b72fd7ba963b583b834
Isaias Santos <isaiassousa032@gmail.com>
2024-08-24 13:56:16 -0300

    band of name generator

---
+++ band_name_generator/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band of Name Generator.")
+city = input("Whats's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
